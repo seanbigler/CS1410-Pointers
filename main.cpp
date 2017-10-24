51977cc4841e11a367094d7c498189ba060d9e36
w01164264 <seanbigler@mail.weber.edu>
2017-10-24 13:23:34 -0600

    Added functionality to increment by address.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
+const int LENGTH = 5;
+void Increment(int* inc);
+
+
 
 int main()
 {
     int i = 9;
-    int j = 10;
     int* pi;
+
+    int iarr[LENGTH] = {4, 8, 15, 16, 23};
+    int* piarr;
+    piarr =  iarr;  // Get address of the array, or element 0
+    // Do not use & operator on arrays
+
     cout << "value of i is: " << i << endl;
     cout << "address of i is: " << &i << endl;
 
@@ -26,6 +35,29 @@ int main()
     cout << "value of i is: " << i << endl;
     cout << "value of the value living at address pi is: " << *pi << endl;  // *pi displays value at that address
 
+    for (int i = 0; i < LENGTH; i++)
+    {
+        cout << iarr[i] << endl;
+    }
+    for (int i = 0; i < LENGTH; i++)
+    {
+        cout << *(iarr+i) << endl;
+    }
+    for (int i = 0; i < LENGTH; i++)
+    {
+        cout << *(piarr++) << endl;
+    }
+
+    Increment(&i);
+
+    cout << "value of i is: " << i << endl;
+    cout << "value of the value living at address pi is: " << *pi << endl;  // *pi displays value at that address
+
 
     return 0;
+}
+
+void Increment(int *inc)
+{
+    *inc += 1;
 }
